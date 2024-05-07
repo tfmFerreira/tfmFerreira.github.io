450b0eb972c43925291e9fabec1578115b408138
Tiago Ferreira <tiagoferreira@Tiagos-MacBook-Pro.local>
2024-05-07 16:32:38 +0200

    draft

--- header.js
+++ header.js
@@ -1,16 +1,17 @@
 document.write(`
     <header style="text-align: center;">
-        <h1>Tiago Mendes Ferreira</h1>
+        <h1>Tiago Mendes Ferreira 
+</h1>
         <nav>
             <ul style="text-align: center;">
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About</a></li>
                 <li><a href="research.html">Research</a></li>
                 <li><a href="publications.html">Publications</a></li>
-                <li><a href="team.html">Team</a></li>
                 <li><a href="contact.html">Contact</a></li>
             </ul>
         </nav>
+
     </header>
 `);
 
