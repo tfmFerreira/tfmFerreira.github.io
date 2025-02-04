914ae2694db6f54153ac6d774e85f304aeaff66c
Tiago Mendes <ferreira.t.f.m@gmail.com>
2025-02-04 15:09:14 +0100

    newheader

--- header.js
+++ header.js
@@ -1,11 +1,10 @@
 document.write(`
     <header style="text-align: center;">
-        <h1>Tiago Mendes Ferreira 
+        <h1>Biomembrane Magnetic Resonance Lab
 </h1>
         <nav>
             <ul style="text-align: center;">
                 <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
                 <li><a href="research.html">Research</a></li>
                 <li><a href="publications.html">Publications</a></li>
                 <li><a href="contact.html">Contact</a></li>
